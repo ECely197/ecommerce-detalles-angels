5dc733dfc00ade7d4ae2b28cd88aa98b8185c7d3
Erick Pinedo <124737187+Erickferpinedo@users.noreply.github.com>
2024-12-19 21:52:15 -0500

    adding more changes to the repo and fixed the cart and the m,ath

--- src/app/components/cart-product/cart-product.component.ts
+++ src/app/components/cart-product/cart-product.component.ts
@@ -2,14 +2,13 @@ import { CurrencyPipe } from '@angular/common';
 import { Component, Input, SimpleChanges, inject } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { CartService } from '../../services/cart.service';
-import { Product } from '../../models/product.model';
 
 @Component({
   selector: 'cart-product',
   standalone: true,
   imports: [ReactiveFormsModule, CurrencyPipe],
   templateUrl: './cart-product.component.html',
-  styleUrl: './cart-product.component.css'
+  styleUrls: ['./cart-product.component.css']
 })
 export class CartProductComponent {
   private cartService = inject(CartService)
@@ -21,6 +20,7 @@ export class CartProductComponent {
 
   ngOnChanges(changes: SimpleChanges) {
     if (changes['product'] && this.product) {
+      // Asignamos la cantidad proveniente del producto en el carrito
       this.productQuantity.setValue(this.product.quantity);
     }
   }
