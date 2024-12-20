66eb71829d819f9704c5e3f0e211f196294daa6b
Erick Pinedo <124737187+Erickferpinedo@users.noreply.github.com>
2024-12-19 21:52:15 -0500

    adding more changes to the repo and fixed the cart and the m,ath

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, inject, Signal, signal } from '@angular/core';
+import { Component, inject, computed } from '@angular/core';
 import { CommonModule, CurrencyPipe } from '@angular/common';
 import { CartService } from '../../services/cart.service';
 import { CartProductComponent } from '../cart-product/cart-product.component';
@@ -14,14 +14,27 @@ import { RouterLinkWithHref } from '@angular/router';
     RouterLinkWithHref,
   ],
   templateUrl: './cart.component.html',
-  styleUrl: './cart.component.css',
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent {
   private cartService = inject(CartService);
 
+  // cart es una señal que retorna un Map<string,Product>
   cart = this.cartService.products;
-  totalPrice = this.cartService.total;
   cartVisibility = this.cartService.cartVisibility;
+
+  // Convertimos los valores del Map en un array de productos.
+  cartProducts = computed(() => Array.from(this.cart().values()));
+
+  // Computamos la cantidad total de items sumando las cantidades de cada producto
+  totalQuantity = computed(() => {
+    return this.cartProducts().reduce((acc, product) => acc + product.quantity, 0);
+  });
+
+  // Computamos el precio total multiplicando precio * cantidad de cada producto y sumando todos
+  totalPrice = computed(() => {
+    return this.cartProducts().reduce((acc, product) => acc + (product.precio * product.quantity), 0);
+  });
 
   handleCartClick(): void {
     this.cartService.toggleCartVisibility();
